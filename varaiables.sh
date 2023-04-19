6dd5ae5d148d512db9bacc4fcfff149d9b6effb5
pavanbairu <pavan.bairu1@gmail.com>
2023-04-19 23:26:01 +0530

    added functions and conditions

--- varaiables.sh
+++ varaiables.sh
@@ -3,4 +3,14 @@ echo path is ${script}
 
 script_path=$(dirname "$script")
 echo directory of variables.sh is = $script_path
+
+a=12
+b=13
+
+echo the value of a is $1
+echo the value of b is $2
+echo all values $*
+echo no of values $#
+
+# running the script as bash variables.sh 12 13
 exit
